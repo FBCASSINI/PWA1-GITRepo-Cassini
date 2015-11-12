08a8943848c9569658472d2201e35c68c8d454b7
Flavio Cassini <fbcassini@fullsail.edu>
2015-11-12 04:44:18 -0500

    Stuck
    stuck

---
+++ wk3/Cassini_Flavio_Debug/js/search.js
@@ -0,0 +1,136 @@
+/*
+
+Flavio Cassini
+10/31/2015
+Class 1511
+Instructor Garlic
+Assignment Goal 3 DEBUG
+PWA-O
+ */
+
+
+
+// Create privatized scope using a self-executing function
+(function(){ // anonymous function
+
+	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
+	var resultsDIV = document.getElementById("results"),//needs semicolon, instead of coma. variable resultsdiv
+		searchInput = document.forms[0].search //Missing a "var" before search input. needs semi coloin instead of comma, variable search input
+		currentSearch = ''//missing "var" before currentSearch needs semi colon at the end, Currentsearch may not be needed
+		; //This semi clon can be used for the line above
+
+	// Validates search query
+	var validqte = function(query){ //needs only one "=" sign, variable validqte.
+
+		// Trim whitespace from start and end of search query
+		while(query.charAt(0) === " "){ //while loop to trim whitespace
+			query = query.substring(1, query.length);
+		}; // unnecessary semi colon
+		while(query.charAt(query.length-1) === "") {//while loop to trim white space
+			query = query.substring(0, query.length - 1);
+		}; //needs bracket } to close  while loop. unnecessary semi colon
+
+		// Check search length, must have 3 characters
+		if(query.length < 3){ //if query.length is greater then 3 call alertbox
+			alert("Your search query is too small, try again."); //needs ending quatation after try again. //alert phrase
+
+			// (DO NOT FIX THE LINE DIRECTLY BELOW)
+			searchInput.focus();
+			return;
+			//returns out of the function
+		}; //unnecessary semi colon
+
+		search(query);
+
+	};
+	// Finds search matches
+	var search = function(query) { //Needs bracket } varriable search
+
+		// split the user's search query string into an array
+		var queryArray = query.split(" ");   //variable queryArray splits assigning query join
+
+		// array to store matched results from database.js
+		var results = []; //variable array to to match reults from database.js
+
+		// loop through each index of db array
+		for(var i= 0, j=db.length; i<j; i++) { // for loop is i<j if not +1 loop
+
+			// each db[i] is a single video item, each title ends with a pipe "|"
+			// save a lowercase variable of the video title
+			var dbTitleEnd = db[i].indexOf('|'); //seperates the video items by a | pipe
+			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd); //saves var dbitem to lowercase
+
+			// loop through the user's search query words
+			// save a lowercase variable of the search keyword
+			for (var ii = 0, jj = queryArray.length; ii < jj; ii++) { //for loop to throgh query search words , 11<jj loop keep looping until a match.
+				var qitem = queryArray[ii].tolowercase();
+
+				// is the keyword anywhere in the video title?
+				// If a match is found, push full db[i] into results array
+				var compare = dbitem.indexOf(qitem); //variable compare to find any search term matches
+				if (compare !== -1) {
+					results.push(db[i]);
+				}
+				; //unnecessary semi colon
+			}
+			;  //Needs } to close the loop // unnecessary semi colon
+		}; //needs bracket } to close the loop // unnecessary semi colon
+
+
+		results.sort();
+
+		// Check that matches were found, and run output functions
+		if(results.length === 0){ //if no mtach jump to var no match
+			noMatch();
+		}else{
+			showMatches(results); //else show matches
+		}; //unnecessary semi colon
+	}; // unnecessary semi colon
+
+	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
+	var noMatch = function(){// var nomatch prints out term
+		var html = ''+
+			'<p>No Results found.</p>'+
+			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>';//Semi colon is in wrong place needs to be in front of try and after idea.
+
+		resultsDIV.innerHTML = html;
+	}; //unnecessary semi colon
+
+	// Put matches into page as paragraphs with anchors
+	var showMatches = function(results){//var showmatches puts matches into a page with anchors
+
+		// THE NEXT 4 LINES ARE CORRECT.
+		var html = '<p>Results</p>',//var html, title, url. print out results
+			title,
+			url
+			;
+
+		// loop through all the results search() function
+		for(var i=0, j=results.length; i<j; i++){ //for loop i<j loop again until a match
+
+			// title of video ends with pipe
+			// pull the title's string using index numbers
+			titleEnd = results[i].indexOf('|'); //title of video ends with pipe
+			title = results[i].substring(0, titleEnd);
+
+			// pull the video url after the title
+			url = results[i].substring(results[i].indexOf('|')+1, results[i].length);//pull the video after the title using "|" pipe
+
+			// make the video link - THE NEXT LINE IS CORRECT.
+			html += '<p><a href=' + url + '>' + title + '</a></p>'; //Make video link using concatination
+		};
+		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+	};
+
+	// The onsubmit event will be reviewed in upcoming Course Material.
+	// THE LINE DIRECTLY BELOW IS CORRECT
+	document.forms[0].onsubmit = function() {
+		var query = searchInput.value;
+		validqte(query);
+
+		// return false is needed for most events - this will be reviewed in upcoming course material
+		// THE LINE DIRECTLY BELOW IS CORRECT
+		return false;
+
+	}
+}());
---
+++ wk3/goal4_assign_duel3/js/main.js
@@ -0,0 +1,86 @@
+/**
+ * Duel Fight Game - FINISHED
+ * Flavio Cassini
+ * Date: 11/04/2015
+ * Instructor Garlic
+ * Class 1511
+ * Assignment Duel 2
+
+ Assignment 1
+ Part 1/3 of series
+*/
+
+// self-executing function
+(function(){  // self executing function
+
+    console.log("FIGHT!!!"); //console.log the word fight
+
+    //player name
+    var fighterOne = {'name':'Spiderman', 'damage':20, 'health':100};  //variable playeronename is spiderman this will display spiderman when javascript loads
+    var fighterTwo = {'name':'Batman','damage':20, 'health':100};   //variable payertwoname = batman. this will display spiderman when javascript loads
+
+    //player damage
+    //var player1Damage = 20; //variable player1damage =20. this varaible will deduct 20points when called.
+    //var player2Damage = 20; //variable player2damage = 20. this variable will deduct 20points when called upon.
+
+    //player health
+   // var playerOneHealth = 100; //variable player health = 100. The variable will start the players at 100points
+    //var playerTwoHealth = 100;
+
+    //initiate round
+    var round=0;  //This variable will start the round and keep track of it.
+
+    function fight(){ //This function will excute fight when called up;on.
+      var kabal = document.getElementById("kabal")
+        //fighterOne = kabal;
+      var kratos = document.getElementById("kratos")
+        //fighterTwo = kratos;
+        //alert(fighterOne.name +":"+fighterOne.health+"  *START*  "+fighterTwo.name+":"+fighterTwo.health); //This alert box will pop up when javascript is run and will notify in the box the players name and  health with the start button in the middile using string conatination.
+        for (var i = 0; i < 10; i++) //for loop executes and checks if it looped 10 times meaning 10 rounds then class winner check. variable i = rounds. i < 10 notifies the script if 10 has been reached, i++ adds to i by 1 point every round until reached 10 rounds.
+        {
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = fighterOne.damage * .5; // minimum damage is .5 to player one from 20 = 10 . to made a between 10-20 damage point per round.
+            var minDamage2 = fighterTwo.damage * .5;   //minimum damage is .5 to player two from 20 = 10
+            var f1 = Math.floor(Math.random()*(fighterOne.damage-minDamage1)+minDamage1); // this variable executes when playerOneHealth-=f1 calls it. it will generate a random number from math.random for deduction in damage points. Then player1/2damage-mindame1/1 will be executing then + mindamage to excute damage between 10- 20
+            var f2 = Math.floor(Math.random()*(fighterTwo.damage-minDamage2)+minDamage2);//this variable executes when playertwoHealth-=f2 calls it. it will generate a random number from math.random for deduction in damage points. Then player1/2damage-mindame1/1 will be executing then + mindamage to excute damage between 10- 20
+
+            //inflict damage
+            fighterOne.health-=f1; //This variable will execute var f1 using math.random to genearte and conflict damage to playeronehealth
+            fighterTwo.health-=f2; //this variable will execute var f2 using math.random to generate and conflict damgage too playertwohealth
+
+            console.log(fighterOne.name+": "+fighterOne.health + " " + fighterTwo.name+":"+fighterTwo.health); // this will console.log playeronename and playertwoname plus playeronehealth and player2helath plus concatination.
+
+            //check for victor
+            var result = winnerCheck(); //function fight calls on winner check
+            console.log(result);
+            if (result==="no winner")//if variable results === no winner
+            {
+                round++;// add 1 more to the round when executed.
+                alert(fighterOne.name+":"+fighterOne.health+"  *ROUND "+round+" OVER"+"*  "+fighterTwo.name+":"+fighterTwo.health); // alert box to notify if round is over plus what round your on.
+
+            } else{ //else
+                alert(result); //notifys who won
+                break; //break outside the code function fight and exits
+            };
+
+          };
+    };
+
+    function winnerCheck(){
+        var result="no winner";
+        if (fighterOne.health<1 && fighterTwo.health<1) //conditional statement to compare playeronehealth and playertwo health is less then one
+        {
+            result = "You Both Die"; //candition is true they both die
+        } else if(fighterOne.health<1){//else if playerone health  is less then 1 , playertwo wins
+            result =fighterTwo.name+" WINS!!!"// Playertwo wins
+        } else if (fighterTwo.health<1)////else if playertwo health  is less  then 1 playerone wins
+        {
+            result = fighterOne.name+" WINS!!!" //playerone wins
+        };
+       return result; //  exits out of function returns result to winnercheck.
+    };
+
+    /*******  The program gets started below *******/
+    fight(); //Calls the fight function and begins program.
+
+})();
